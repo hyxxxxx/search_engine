b688879b2d3648af028217a230bcb95628cf8dac
yasin <357519203@qq.com>
2019-03-08 18:21:02 +0800

    upload

--- src/main/java/com/dbc/exert/Collector.java
+++ src/main/java/com/dbc/exert/Collector.java
@@ -1,26 +1,41 @@
 package com.dbc.exert;
 
 import com.dbc.exert.net.HttpHelper;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Collector implements Runnable {
 
-    public static Queue<String> queue = new LinkedBlockingQueue<>();
+    public static Queue<String> links = new LinkedBlockingQueue<>();
+    public static BloomFilter<String> linkFilter = BloomFilter.create(Funnels.stringFunnel(Charset.defaultCharset()), 100);
 
     @Override
     public void run() {
 
-        String url = queue.poll();
-        String body;
+        String url = links.poll();
+        String htmlStr = "";
         try {
-            body = HttpHelper.sendGet(url);
+            htmlStr = HttpHelper.sendGet(url);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+        Document document = Jsoup.parse(htmlStr);
+        Element body = document.body();
+        Elements href = body.getElementsByAttribute("href");
+        for (Element element : href) {
+            String link = element.attr("href").trim();
+            if (link.startsWith("http")) {
+                links.offer(link);
+            }
+        }
     }
 }
--- src/main/java/com/dbc/exert/Main.java
+++ src/main/java/com/dbc/exert/Main.java
@@ -1,29 +1,44 @@
 package com.dbc.exert;
 
 import com.dbc.exert.net.HttpHelper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
 
     public static void main(String[] args) throws InterruptedException {
 
-        try {
-            String html = HttpHelper.sendGet("https://www.sina.com.cn/");
-            System.out.println(html);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        ExecutorService service = Executors.newFixedThreadPool(10);
 
-        ExecutorService service = Executors.newFixedThreadPool(10);
+//        Collector.links.offer("https://www.sina.com.cn/");
 
-        service.submit(new Collector());
+//        service.submit(new Collector());
 //        service.submit(new Analyzer());
 //        service.submit(new Indexer());
 //        service.submit(new Querier());
+
+        String htmlStr = "";
+        try {
+            htmlStr = HttpHelper.sendGet("https://www.sina.com.cn/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Document document = Jsoup.parse(htmlStr);
+        Element body = document.body();
+        Elements href = body.getElementsByAttribute("href");
+        for (Element element : href) {
+            System.out.println(element.attr("href"));
+        }
+
 
     }
 
